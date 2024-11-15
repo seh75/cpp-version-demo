a981f1f8adb5d2ac06d20eb1b78109187f9cd788
fhzn.seh <seh2023@fhzn.com>
2024-11-15 16:55:32 +0800

    Version 4: Added subtract() function for subtraction.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,17 @@ int add(int a, int b)
     return a + b;
 }
 
+int subtract(int a, int b)
+{
+    return a - b;
+}
+
 int main()
 {
     int num1, num2;
     std::cout << "Enter two numbers: ";
     std::cin >> num1 >> num2;
     std::cout << "Addition of " << num1 << " and " << num2 << " is: " << add(num1, num2) << std::endl;
+    std::cout << "Subtraction of " << num1 << " and " << num2 << " is: " << subtract(num1, num2) << std::endl;
     return 0;
 }
